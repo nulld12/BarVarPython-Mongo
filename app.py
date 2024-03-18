01e039dc9e2390802e406f866d94360230dceedd
David Rodriguez <dtuma4l3@gmail.com>
2024-03-18 13:51:27 +0100

    finalizado

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 import sys
+import uuid
 
 
 def menu():
@@ -15,6 +16,7 @@ def menu():
     "3.Reservar mesa"+ "\n"+ 
     "4.Consumiciones mesa"+"\n"+
     "5.Carta restaurante")
+
     print()
     dato = input("Por favor, Escoge la Opcion que seas: ")
     if dato == "1":
@@ -31,22 +33,33 @@ def menu():
         return sys.exit("Vuelva Pronto")
 
 def ver_mesas():
-    #Guarda las mesas en la variable
+    # Guarda las mesas en la variable
     mesitas = mesas.find()
     for mesa in mesitas:
-        #muestra cada dato de la mesa por separado
+        # Muestra cada dato de la mesa por separado
         print("Número de Mesa:", mesa["numeroMesa"])
         print("Ubicación:", mesa["ubicacion"])
         print("Número de Comensales:", mesa["nComensales"])
         print("Estado:", mesa["estado"])
         print("Número de Reserva:", mesa["nReserva"])
-        #espacio entre las mesas a la hora de mostrarlas
+        # Espacio entre las mesas a la hora de mostrarlas
         print()
+    menu()
 
 
 def cambiar_estado():
-    #muestra las mesas
-    ver_mesas()
+    # Muestra las mesas
+    mesitas = mesas.find()
+    for mesa in mesitas:
+        # Muestra cada dato de la mesa por separado
+        print("Número de Mesa:", mesa["numeroMesa"])
+        print("Ubicación:", mesa["ubicacion"])
+        print("Número de Comensales:", mesa["nComensales"])
+        print("Estado:", mesa["estado"])
+        print("Número de Reserva:", mesa["nReserva"])
+        # Espacio entre las mesas a la hora de mostrarlas
+        print()
+    print()
     # Solicita el ID de la mesa
     id_mesa = int(input("Ingrese el ID de la mesa que desea actualizar: "))
 
@@ -64,19 +77,27 @@ def cambiar_estado():
             mesas.update_one({"numeroMesa": id_mesa}, {"$set": {"estado": nuevo_estado}})
             print("El estado de la mesa se ha actualizado correctamente.")
         else:
-            print("Estado inválido. Los estados válidos son: libre, ocupada, pagada, servida, proceso o bloqueada .")
+            print("Estado inválido. Los estados válidos son: libre, ocupada, pagada, servida, proceso o bloqueada.")
     else:
         print("No se encontró ninguna mesa con ese ID.")
+    
+    menu()  
 
-
+#genera un nuevo uuid de 4 y coge
+def generate_and_extract_prefix():
+    reservation_id = str(uuid.uuid4())
+    # Coge los caracteres antes del primer guion
+    prefix = reservation_id.split('-')[0]
+    return prefix
+    
 def reservar_mesa():
     # Solicita el número de personas
     n_personas = int(input("Ingrese el número de personas para la reserva: "))
 
-    # busca mesas disponibles para el número de personas ingresado
-    mesas_disponibles = mesas.find({"$or": [{"estado": "libre"}, {"estado": "ocupada"}], "nComensales": {"$gte": n_personas}})
+    # Busca mesas disponibles para el número de personas ingresado y con estado "libre"
+    mesas_disponibles = mesas.find({"estado": "libre", "nComensales": {"$gte": n_personas}})
     
-    # Mostrar mesas disponibles
+    # Mostrar mesas disponibles para la reserva
     print("Mesas disponibles para la reserva:")
     for mesa in mesas_disponibles:
         print("ID de Mesa:", mesa["numeroMesa"])
@@ -88,27 +109,49 @@ def reservar_mesa():
     # Solicita el ID de la mesa para la reserva
     id_mesa = int(input("Ingrese el ID de la mesa que desea reservar: "))
 
-    # Verifica si la mesa está disponible y actualizar su estado a reservada
-    mesa_reserva = mesas.find_one({"numeroMesa": id_mesa, "$or": [{"estado": "libre"}, {"estado": "ocupada"}], "nComensales": {"$gte": n_personas}})
+    # Verifica si la mesa está disponible y su estado es "libre" para reservar
+    mesa_reserva = mesas.find_one({"numeroMesa": id_mesa, "estado": "libre", "nComensales": {"$gte": n_personas}})
     if mesa_reserva:
-        mesas.update_one({"numeroMesa": id_mesa}, {"$set": {"estado": "reservada", "nReserva": "ID_RESERVA"}})
+        # Actualizar estado de la mesa a "reservada" y asignar un número de reserva
+        mesas.update_one({"numeroMesa": id_mesa}, {"$set": {"estado": "reservada", "nReserva": generate_and_extract_prefix()}})
         print("La mesa se ha reservado correctamente.")
     else:
         print("La mesa no está disponible para la reserva.")
+    menu()
 
-def ver_menu_mesa():
-    return "hola"
+def ver_menu_mesa(): 
+    numero_mesa = input("Ingrese el número de mesa: ")
+    numero_mesa = int(numero_mesa)
+    print(numero_mesa)
+    pedidos_mesa = pedidos.find({'nMesa': numero_mesa})
+    # Mostrar los pedidos encontrados
+    print("Pedidos encontrados para la mesa", numero_mesa, ":")
+    print("------------------------------------------------------------")
+    for pedido in pedidos_mesa:
+        print("Pedido ID:", pedido['_id'])
+        print("Fecha:", pedido['fecha'])
+        print("Importe:", pedido['importe'])
+        print("Número de Mesa:", pedido['nMesa'])
+        print("Detalles del pedido:")
+        for detalle_pedido in pedido['pedido']:
+            print("  Nombre:", detalle_pedido['nombre'])
+            print("  Precio:", detalle_pedido['precio'])
+            print("  Cantidad:", detalle_pedido['cantidad'])
+            print("  ID:", detalle_pedido['_id'])
+            print("------------------------------------------------------------")
+    print("\n")  
+    menu()
+
 
 def carta():
     print()
     """
     Carta de restaurante
     """
-    print("Bar Var: Menu Restaurante"+"\n"+
+    print("Bar Var: Carta Restaurante"+"\n"+
     "1.Ver Consumiciones"+"\n"+
     "2.Agregar consumición a la carta"+"\n"+
-    "3.Eliminar consumición"+ "\n"+ 
-    "4.Salir al menu de camarero")
+    "3.Salir al menu de camarero")
     print()
     dato = input("Por favor, Escoge la Opcion que seas:")
     if dato == "1":
@@ -116,8 +159,6 @@ def carta():
     elif dato == "2":
         return agregar_consumicion()
     elif dato == "3":
-        return eliminar_consumicion()
-    elif dato == "4":
         return menu()
     else :
         return sys.exit("Vuelva Pronto")
@@ -141,49 +182,68 @@ def ver_consumiciones():
                         print()
     else:
         print("No se encontró ningún menú en la base de datos.")
+    carta()
 
 def agregar_consumicion():
-    
-    # tipo_alimento = input("¿Qué tipo de alimento quieres añadir (postre, bebida o comida)? ").lower()
-    # # Verificar si el tipo de alimento es válido
-    # if tipo_alimento not in ["postre", "bebida", "comida"]:
-    #     print("Tipo de alimento no válido.")
-    #     return
-    
-    # # Solicitar los datos del alimento
-    # nuevo_alimento = {}
-    # nuevo_alimento["id"] = int(input("ID del alimento: "))
-    # nuevo_alimento["nombre"] = input("Nombre del alimento: ")
-    # nuevo_alimento["precio"] = float(input("Precio del alimento: "))
-    # nuevo_alimento["imagen"] = input("URI de la imagen del alimento: ")
-    
-    # # Verificar si ya existe un alimento con el mismo ID
-    # for alimento_tipo in menuse[0][tipo_alimento + "s"]:
-    #     if alimento_tipo["id"] == nuevo_alimento["id"]:
-    #         print("Ya existe un alimento con ese ID.")
-    #         return
-    
-    # # Añadir el nuevo alimento al tipo correspondiente en la colección
-    #     menuse[0][tipo_alimento + "s"].insertOne(nuevo_alimento)
-    # print("Alimento añadido correctamente.")
-    return "hola"
 
-def eliminar_consumicion():
-    return "hola"
-
-
+    tipo_elemento = input("¿Qué tipo de elemento desea agregar (bebida, comida o postre)? ").lower()
+    
+    # Solicitar los datos del nuevo elemento
+    id_elemento = int(input("Ingrese el id del nuevo elemento: ").strip())
+    nombre = input("Ingrese el nombre del nuevo elemento: ").strip()
+    precio = float(input("Ingrese el precio del nuevo elemento: ").strip())
+    imagen = input("Ingrese la URL de la imagen del nuevo elemento: ").strip()
+    
+    # Verificar si el ID ya existe en alguna de las subcolecciones
+    if tipo_elemento == "bebida":
+        if menuse.find_one({"bebidas.id": id_elemento}):
+            print("Ya existe un elemento en las bebidas con el mismo ID.")
+            return
+    elif tipo_elemento == "comida":
+        if menuse.find_one({"comidas.id": id_elemento}):
+            print("Ya existe un elemento en las comidas con el mismo ID.")
+            return
+    elif tipo_elemento == "postre":
+        if menuse.find_one({"postres.id": id_elemento}):
+            print("Ya existe un elemento en los postres con el mismo ID.")
+            return
+    else:
+        print("Tipo de elemento inválido. Los tipos válidos son: bebida, comida o postre.")
+        return
+    
+    # Crear el nuevo objeto
+    nuevo_elemento = {
+        "id": id_elemento,
+        "nombre": nombre,
+        "precio": precio,
+        "imagen": imagen
+    }
+    
+    # Determinar la colección correspondiente y agregar el nuevo elemento
+    if tipo_elemento == "bebida":
+        menuse.update_one({}, {"$push": {"bebidas": nuevo_elemento}})
+        print("Elemento añadido correctamente a las bebidas.")
+    elif tipo_elemento == "comida":
+        menuse.update_one({}, {"$push": {"comidas": nuevo_elemento}})
+        print("Elemento añadido correctamente a las comidas.")
+    elif tipo_elemento == "postre":
+        menuse.update_one({}, {"$push": {"postres": nuevo_elemento}})
+        print("Elemento añadido correctamente a los postres.")
+    else:
+        print("Tipo de elemento inválido. Los tipos válidos son: bebida, comida o postre.")
+    carta()
 
 
 
 uri = "mongodb+srv://david:Q123321q@cluster0.nmh0cmf.mongodb.net/?retryWrites=true&w=majority"
-#Crea un nuevo cliente para conectarse       
+# Crea un nuevo cliente para conectarse  
 client = MongoClient(uri,ssl=True,tlsAllowInvalidCertificates=True)
 database: Database= client.get_database("BarVar")
 mesas:Collection = database.get_collection("mesas")
 menuse:Collection = database.get_collection("menu")
 pedidos:Collection = database.get_collection("pedidos")
 tickets:Collection = database.get_collection("tickets")
-#Hace un ping para comprobar el estado
+# Hace un ping para comprobar el estado
 try:
     client.admin.command('ping')
     print("Conexion Correcta")
